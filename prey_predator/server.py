7ec6e1e3804a9cc9b5e96a84d27f2772eba16035
Fernando Matsumoto <ferkmatsumoto@gmail.com>
2023-03-03 14:17:57 +0100

    Update display

--- prey_predator/server.py
+++ prey_predator/server.py
@@ -6,8 +6,9 @@ from prey_predator.agents import Wolf, Sheep, GrassPatch
 from prey_predator.model import WolfSheep
 
 
-COLOR_WOLF = "#AA0000"
-COLOR_SHEEP = "#666666"
+COLOR_WOLF = "#CC0000"
+COLOR_SHEEP = "#483D8B"
+COLOR_GRASS = "#7FFF00"
 
 
 def wolf_sheep_portrayal(agent):
@@ -20,7 +21,7 @@ def wolf_sheep_portrayal(agent):
         portrayal = {
             "Shape": "circle",
             "Filled": "true",
-            "Color": "blue",
+            "Color": COLOR_SHEEP,
             "r": 0.3,
             "Layer": 2
         }
@@ -28,20 +29,31 @@ def wolf_sheep_portrayal(agent):
         portrayal = {
             "Shape": "circle",
             "Filled": "true",
-            "Color": "red",
+            "Color": COLOR_WOLF,
             "r": 0.5,
             "Layer": 1
         }
 
-    elif type(agent) is GrassPatch and agent.fully_grown:
-        portrayal = {
-            "Shape": "rect",
-            "Filled": "true",
-            "Color": "green",
-            "w" : 0.7,
-            "h" : 0.7,
-            "Layer": 0
-        }
+    elif type(agent) is GrassPatch:
+        if agent.fully_grown:
+            portrayal = {
+                "Shape": "rect",
+                "Filled": "false",
+                "Color": COLOR_GRASS,
+                "w" : 0.9,
+                "h" : 0.9,
+                "Layer": 0
+            }
+        else:
+            p = 1 - agent.countdown / agent.model.grass_regrowth_time
+            portrayal = {
+                "Shape": "rect",
+                "Filled": "false",
+                "Color": COLOR_GRASS,
+                "w" : 0.7 * p,
+                "h" : 0.7 * p,
+                "Layer": 0
+            }
 
     return portrayal
 
